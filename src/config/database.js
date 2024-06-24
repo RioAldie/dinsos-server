704d470515b1b1253ad8479319386ad8157fefc5
rioaldie <rioaldierwanto@gmail.com>
2024-06-24 19:29:48 +0700

    ready to deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
+dotenv.config();
 const database_url = 'mongodb://127.0.0.1:27017/db_lapakktpa';
 
-export const database = mongoose.connect(database_url);
+export const database = mongoose.connect(
+  process.env.MONGODB_CONNECT_URL
+);
